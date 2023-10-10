c62c341d0f25c77c9d1d185ea4dec8a9f3d45f95
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2023-10-10 11:25:08 +0900

    2023/10/10 margin-bottom:5px;

--- act100/resources/views/advisorsfee/input.blade.php
+++ act100/resources/views/advisorsfee/input.blade.php
@@ -5,7 +5,7 @@
     {{-- <h2>顧問料一覧</h2> --}}
     <div class="text-right">
         {{-- <a class="btn btn-success btn-sm mr-auto" href="{{route('advisorsfee.create')}}">新規登録</a> --}}
-        <a id="start2" class="btn btn-success btn-sm mr-auto" href="{{route('excelexp')}}">請求書作成</a>
+        <a id="start2" style="margin-bottom:5px;" class="btn btn-success btn-sm mr-auto" href="{{route('excelexp')}}">請求書作成</a>
     </div>
 
     <div class="row">
