c1715994fb3e302454c1851c2ef5d3e8b7663168
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2023-11-03 18:24:10 +0900

    2023/11/03 LINE MESSAGE

--- act100/app/Http/Controllers/LineWebhookController.php
+++ act100/app/Http/Controllers/LineWebhookController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 use App\Models\Line_Message;
