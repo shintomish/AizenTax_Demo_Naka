ccc0ebac8472ee67892cbdcc2f03ac4e7db0a776
shintomish <shintomi.sh@gmail.com>
2025-08-24 11:35:25 +0900

    作業内容の説明

--- act100_School/resources/views/components/toastr.blade.php
+++
@@ -1,8 +0,0 @@
-@if($toastr = session('toastr'))
-<script>
-    window.onload = function(){
-        document.querySelector('script[src="{{ asset("js/jquery-3.6.0.min.js") }}"]');
-        toastr.{{ $toastr['type'] }}('{{ $toastr["text"] }}');
-    }
-</script>
-@endif
